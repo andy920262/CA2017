dc2815afd9cff308dd37963e37a9839cc6f5b484
henlium <b04902064@ntu.edu.tw>
2017-11-28 23:16:54 +0800

    ALU_Control updated.

--- project1/src/ALU_Control.v
+++ project1/src/ALU_Control.v
@@ -13,18 +13,20 @@ reg				[2:0]	tmp;
 assign ALUCtrl_o = tmp;
 
 always @(*) begin
-	if (ALUOp_i == 2'b01)
-		tmp = 3'd2;
-	
-	else
-		case (funct_i)
-			6'b100100:	tmp = 3'd0;
-			6'b100101:	tmp = 3'd1;
-			6'b100000:	tmp = 3'd2;
-			6'b100010:	tmp = 3'd3;
-			6'b011000:	tmp = 3'd4;
-		endcase
-
+	case (ALUOp_i)
+		2'b01:
+			tmp = 3'd2;
+		2'b00: // R-type
+			case (funct_i)
+				6'b100100:	tmp = 3'd0; // and
+				6'b100101:	tmp = 3'd1; // or
+				6'b100000:	tmp = 3'd2; // plus
+				6'b100010:	tmp = 3'd3; // minus
+				6'b011000:	tmp = 3'd4; // multiply
+			endcase
+		2'b10: // beq
+			tmp = 3'd3
+	endcase
 end
 
 endmodule
