98b929929a3b577078a5cd8d121d0ad33940cf75
andy920262 <920262@gmail.com>
2017-11-28 23:05:44 +0800

    remove ExtOp

--- project1/src/Control.v
+++ project1/src/Control.v
@@ -9,37 +9,36 @@ module Control
 	MemRead_o,
 	Branch_o,
 	Jump_o,
-	ExtOp_o,
     ALUOp_o
 );
 
 input		[5:0]	Op_i;
-output		[2:0]	ALUOp_o;
+output		[1:0]	ALUOp_o;
 output				RegDst_o, ALUSrc_o, MemtoReg, RegWrite_o;
-output				MemWrite_o, MemRead_o, Branch_o, Jump_o, ExtOp_o;
-reg			[12:0]	tmp;
+output				MemWrite_o, MemRead_o, Branch_o, Jump_o;
+reg			[11:0]	tmp;
 assign { RegDst_o, ALUSrc_o, MemtoReg, RegWrite_o,
-	MemWrite_o, MemRead_o, Branch_o, Jump_o, ExtOp_o, ALUOp_o } = tmp;
+	MemWrite_o, MemRead_o, Branch_o, Jump_o, ALUOp_o } = tmp;
 
 always @(*) begin
 	case (Op_i)
 		6'b000000: begin // r-type
-			tmp = 13'b10010000x000 
+			tmp = 11'b1001000000 
 		end
 		6'b001000: begin // addi
-			tmp = 13'b010100000001
+			tmp = 11'b0101000001
 		end
 		6'b100011: begin // lw
-			tmp = 13'b011101001010
+			tmp = 11'b0111010001
 		end
 		6'b101011: begin // sw
-			tmp = 13'bx1x01x001010
+			tmp = 11'bx1x01x0001
 		end
 		6'b000100: begin // beq
-			tmp = 13'bx0x00x10x011
+			tmp = 11'bx0x00x1010
 		end
 		6'b000010: begin // jump
-			tmp = 13'bxxx00x01xxxx
+			tmp = 11'bxxx00x01xx
 		end
 	endcase
 end
