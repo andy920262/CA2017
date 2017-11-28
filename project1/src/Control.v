d5f73dce83cc9bad436f003635f80fe55b0ad46c
Andy <andy@MacBook-Pro.local>
2017-11-28 19:50:08 +0800

    Control.v

--- project1/src/Control.v
+++ project1/src/Control.v
@@ -2,32 +2,43 @@ module Control
 (
     Op_i,
     RegDst_o,
-    ALUOp_o,
     ALUSrc_o, 
-    RegWrite_o
+	MemtoReg,
+    RegWrite_o,
+	MemWrite_o,
+	Branch_o,
+	Jump_o,
+	ExtOp_o,
+    ALUOp_o
 );
 
 input		[5:0]	Op_i;
-output		[1:0]	ALUOp_o;
-output				RegDst_o, ALUSrc_o, RegWrite_o;
-
-reg			[4:0]	tmp;
-assign {ALUOp_o, RegDst_o, ALUSrc_o, RegWrite_o} = tmp;
-
+output		[2:0]	ALUOp_o;
+output				RegDst_o, ALUSrc_o, MemtoReg, RegWrite_o,
+output				MemWrite_o, Branch_o, Jump_o, ExtOp_o;
+reg			[11:0]	tmp;
+assign { RegDst_o, ALUSrc_o, MemtoReg, RegWrite_o,
+	MemWrite_o, Branch_o, Jump_o, ExtOp_o, ALUOp_o } = tmp;
 
 always @(*) begin
 	case (Op_i)
 		6'b000000: begin // r-type
-			tmp[4:3] = 2'b00;
-			tmp[2] = 1'b1;
-			tmp[1] = 1'b0;
-			tmp[0] = 1'b1;
+			tmp = 12'b1001000x000 
 		end
 		6'b001000: begin // addi
-			tmp[4:3] = 2'b01;
-			tmp[2] = 1'b0;
-			tmp[1] = 1'b1;
-			tmp[0] = 1'b1;
+			tmp = 12'b01010000001
+		end
+		6'b100011: begin // lw
+			tmp = 12'b01110001010
+		end
+		6'b101011: begin // sw
+			tmp = 12'bx1x01001010
+		end
+		6'b000100: begin // beq
+			tmp = 12'bx0x0010x011
+		end
+		6'b000010: begin // jump
+			tmp = 12'bxxx0001xxxx
 		end
 	endcase
 end
