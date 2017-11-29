5c80812359d0eb764c28ab5464bf210ddeab2f65
ChenYoga <yogafox41@gmail.com>
2017-11-29 16:59:01 +0800

    rename

--- project1/src/EX_MEM.v
+++ project1/src/EX_MEM.v
@@ -3,12 +3,12 @@ module EX_MEM(
     M_i,
     ALUresult_i,
     WriteData_i,
-    RegRD_i,
+    RegDst_i,
     WB_o,
     M_o,
     ALUresult_o,
     WriteData_o,
-    RegRD_o
+    RegDst_o
 );
 
 // port
@@ -16,19 +16,19 @@ input   [1:0]   WB_i;
 input   [1:0]   M_i;
 input   [31:0]  ALUresult_i;
 input   [31:0]  WriteData_i;
-input   [4:0]   RegRD_i;
+input   [4:0]   RegDst_i;
 output  reg [1:0]   WB_o;
 output  reg [1:0]   M_o;
 output  reg [31:0]  ALUresult_o;
 output  reg [31:0]  WriteData_o;
-output  reg [4:0]   RegRD_o;
+output  reg [4:0]   RegDst_o;
 
 initial begin
     WB_o <= 0;
     M_o <= 0;
     ALUresult_o <= 0;
     WriteData_o <= 0;
-    RegRD_o <= 0;
+    RegDst_o <= 0;
 end
 
 always@(posedge clk_i) begin
@@ -36,7 +36,7 @@ always@(posedge clk_i) begin
     M_o <= M_i;
     ALUresult_o <= ALUresult_i;
     WriteData_o <= WriteData_i;
-    RegRD_o <= RegRD_i;
+    RegDst_o <= RegDst_i;
 end
 
 endmodule // EX_MEM
